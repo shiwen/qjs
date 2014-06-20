ffe673117a43614edcd4625ba4148b69420bbff4
Shiwen Zhang <clydecheung@gmail.com>
2014-06-20 00:00:09 +0000

    Snapshot

--- v4_oneway_main.js
+++ v4_oneway_main.js
@@ -1214,7 +1214,6 @@ $jex.exec(function() {
                     try {
                         var str = request.responseText;
                         if (str) {
-                            str = $jex.trim(str);
                             if (str.charAt(0, 1) == "{") {
                                 str = "(" + str + ")";
                             }
@@ -5062,9 +5061,6 @@ VendorEntity.prototype.isWorking = function() {
 };
 VendorEntity.prototype.getStatus = function() {
     return this.dataSource().status;
-};
-VendorEntity.prototype.info = function() {
-    return this.dataSource().info || {};
 };
 VendorEntity.prototype.starRank = function() {
     var a = this.dataSource().star;
